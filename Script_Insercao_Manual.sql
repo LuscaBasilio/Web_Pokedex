0f367cd60baaf840945f9c6087a568b839214142
Lucas Basilio Leite <47511187811@info.local>
2019-04-01 11:29:28 -0300

    Inserção de pokemons e locais manualmente

---
+++ Script_Insercao_Manual.sql
@@ -0,0 +1,11 @@
+USE WEB_POKEDEX
+
+INSERT INTO LOCAIS VALUES('Grassland'), ('Forest'), ('Water''s-edge'), ('Sea'), ('Cave'), ('Mountain'), ('Rough-terrain'), ('Urban'),
+('Rare');
+
+INSERT INTO POKEMON VALUES ('Nº004', 'Charmander', 'Lizard Pokémon', 'From the time it is born,
+a flame burns at the tip of it''s tail. It''s life would end if the flame were to go out', 
+'HT: 0,60 M', 'WT: 8,5 kg', 9), ('Nº005', 'Charmeleon', 'Flare Pokémon', 'It lashes about with it''s 
+tail to knock down it''s foe. It then tears up the fallen opponent with sharp claws', 'HT: 0,94 M', 'WT: 19 kg', 9);
+
+SELECT * FROM POKEMON
