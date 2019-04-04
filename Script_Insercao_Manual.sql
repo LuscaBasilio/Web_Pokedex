b45429ddedc1e0a782e67f72d582aa3238e8f352
lucas leite <luka.leite@live.com>
2019-04-03 22:01:28 -0300

    Inserção manual dos Tipos e tabela de intersecção

--- Script_Insercao_Manual.sql
+++ Script_Insercao_Manual.sql
@@ -8,4 +8,11 @@ a flame burns at the tip of it''s tail. It''s life would end if the flame were t
 'HT: 0,60 M', 'WT: 8,5 kg', 9), ('Nº005', 'Charmeleon', 'Flare Pokémon', 'It lashes about with it''s 
 tail to knock down it''s foe. It then tears up the fallen opponent with sharp claws', 'HT: 0,94 M', 'WT: 19 kg', 9);
 
+INSERT INTO TIPOS VALUES ('Grass'), ('Fire'), ('Water'), ('Rock'), ('Ground'), ('Normal'), ('Fighter'), ('Flying'), ('Electric'), 
+('Ghost'), ('Dark'), ('Bug'), ('Psychic'), ('Poison'), ('Steel'), ('Ice'), ('Dragon');
+
+INSERT INTO TIPO_POKEMON VALUES (1, 2), (2, 2);
+
 SELECT * FROM POKEMON
+SELECT * FROM TIPOS
+SELECT * FROM TIPO_POKEMON
